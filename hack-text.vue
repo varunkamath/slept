dccdb5340eae482ba60eab598325d9f159357201
Varun Kamath <varun.kamath@gmail.com>
2023-06-11 22:31:12 -0500

    Initial commit

---
+++ hack-text.vue
@@ -0,0 +1,22 @@
+<template>
+    <vue-hacker-text text="slept@slept.black" />
+</template>
+
+<script>
+    import Vue from 'vue';
+    import VueHackerText from 'vue-hacker-text';
+
+    export default Vue.extend({
+        components: {
+            VueHackerText
+        },
+    });
+</script>
+
+<style>
+    .hacker-text {
+        font-weight: 600;
+        font-size: 2em;
+        color: red;
+    }
+</style>
